1d6e629bcb0b34a6dff6f929e9b9392434ae7678
Colm Britton <colmjude@gmail.com>
2021-04-13 17:24:02 +0100

    Strip back to use what is available from frontend

--- bin/jinja_setup.py
+++ bin/jinja_setup.py
@@ -3,6 +3,9 @@
 import os
 import jinja2
 
+from digital_land_frontend.filters import make_link
+from digital_land_frontend.markdown.filter import markdown_filter
+
 # output directory
 docs = "docs/"
 
@@ -16,19 +19,29 @@ def render(path, template, **kwargs):
         f.write(template.render(**kwargs))
 
 
-# register templates
-multi_loader = jinja2.ChoiceLoader(
-    [
-        jinja2.FileSystemLoader(searchpath="./templates"),
-        jinja2.PrefixLoader(
-            {
-                "digital-land-frontend": jinja2.PackageLoader("digital_land_frontend"),
-                "govuk-jinja-components": jinja2.PackageLoader(
-                    "govuk_jinja_components"
-                ),
-            }
-        ),
-    ]
-)
+def setup_jinja():
+    # register templates
+    multi_loader = jinja2.ChoiceLoader(
+        [
+            jinja2.FileSystemLoader(searchpath="./templates"),
+            jinja2.PrefixLoader(
+                {
+                    "digital-land-frontend": jinja2.PackageLoader("digital_land_frontend"),
+                    "govuk-jinja-components": jinja2.PackageLoader(
+                        "govuk_jinja_components"
+                    ),
+                }
+            ),
+        ]
+    )
 
-env = jinja2.Environment(loader=multi_loader, autoescape=True)
+    env = jinja2.Environment(loader=multi_loader, autoescape=True)
+
+    # register jinja filters
+    env.filters["make_link"] = make_link
+    env.filters["markdown"] = markdown_filter
+
+    # set variables to make available to all templates
+    env.globals["staticPath"] = "https://digital-land.github.io"
+
+    return env
--- render.py
+++ render.py
@@ -4,31 +4,17 @@ import os
 import markdown
 
 from frontmatter import Frontmatter
-from bin.govukify import govukify_markdown_output
-from bin.jinja_setup import env, render
-from bin.helpers import read_in_json
-from digital_land_frontend.filters import make_link
+from bin.jinja_setup import setup_jinja, render
+from digital_land_frontend.markdown.filter import compile_markdown
 
 from markdown.extensions.toc import TocExtension
 
-# register jinja filters
-env.filters["make_link"] = make_link
-
-# set variables to make available to all templates
-env.globals["staticPath"] = "https://digital-land.github.io"
+# init jinja
+env = setup_jinja()
 
 # init markdown
 md = markdown.Markdown(extensions=[TocExtension(toc_depth="2-3")])
 
-def compile_markdown(md, s):
-    html = md.convert(s)
-    return govukify_markdown_output(html)
-
-# making markdown compiler available to jinja templates
-def markdown_filter(s):
-    return compile_markdown(md, s)
-
-env.filters["markdown"] = markdown_filter
 
 def get_file_content(filename):
     file_content = Frontmatter.read_file(filename)
@@ -36,7 +22,7 @@ def get_file_content(filename):
         "name": file_content["attributes"].get("name"),
         "status": file_content["attributes"].get("status"),
         "frontmatter": file_content["attributes"],
-        "body": compile_markdown(md, file_content["body"]),
+        "body": compile_markdown(file_content["body"], md),
     }
 
 
@@ -45,9 +31,10 @@ def markdown_files_only(files, file_ext=".md"):
 
 
 def add_to_index_list(l, page, path_part):
-    page['url'] = path_part
+    page["url"] = path_part
     l.append(page)
     return l
+
 
 # get templates
 index_template = env.get_template("index.html")
@@ -60,7 +47,9 @@ processed_top_level_pages = []
 for top_level_page in top_level_pages:
     hasMultipleDatasets = False
     processed_page = get_file_content(f"{content_dir}{top_level_page}/index.md")
-    processed_top_level_pages = add_to_index_list(processed_top_level_pages, processed_page, top_level_page)
+    processed_top_level_pages = add_to_index_list(
+        processed_top_level_pages, processed_page, top_level_page
+    )
     render(f"{top_level_page}/index.html", content_template, content=processed_page)
 
 
